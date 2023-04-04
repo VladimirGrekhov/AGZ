8e5280ca7c3d8b7c0d15712ec6d8180c595d9961
GVV777 <GVV777@DESKTOP-2PDREKQ>
2023-04-04 09:17:03 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine("*** Вас приветствует Автоматическая Газовая Защита *** \n ");
+Console.Beep();
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine("1 - Ручной режим проверки.");
+Console.WriteLine("2 - Автоматический режим проверки.");
+DMS DMS1 = new DMS();
+DMS1.print(0.17);
+ class DMS
+{   
+    public void print(double Conc)
+    {
+        Console.ForegroundColor = ConsoleColor.DarkGreen;
+        Console.WriteLine($"Датчик Метана {Conc} %");
+    }
+    
+}
+
